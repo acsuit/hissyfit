8d2afb43add79834d55b567cb3f87fe822868344
Anna Suiter <acsuiter@gmail.com>
2014-04-21 21:53:17 -0500

    just getting into the habit of daily commits

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python
 
 print 'This is HissyFit. Eventually. Once I know what HissyFit actually is.'
+answer = input('What would you like to do? 1, 2, or 3?')
+if answer = 1:
+	print 'You win!'
+elif answer = 2:
+	Print 'You still win!'
+else:
+	Print 'You lose.'
